e00257ed750cdaec150fb3631d32de4e5c52cfbd
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-11-27 17:52:01 +0000

    Add shebang back

--- scripts/jdk-installer.sh
+++ scripts/jdk-installer.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm -rf /tmp/jdk11
 (cd /tmp; curl https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz | gunzip -c | tar xf -)
 mv /tmp/jdk-11.0.1 /tmp/jdk11
