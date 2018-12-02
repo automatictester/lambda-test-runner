1ca3e532f9bb9883f4d69b8b78238bb96cb55711
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-12-02 18:43:33 +0000

    Add missing JAVA_HOME env var for test execution

--- src/test/java/uk/co/automatictester/lambdatestrunner/JdkInstallerTest.java
+++ src/test/java/uk/co/automatictester/lambdatestrunner/JdkInstallerTest.java
@@ -6,14 +6,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertTrue;
 
 public class JdkInstallerTest {
 
     @Test
     public void testInstallJdk() {
         JdkInstaller.installJdk();
-        Path path = Paths.get("/tmp/jdk10");
+        Path path = Paths.get(System.getenv("JAVA_HOME"));
         assertTrue(Files.exists(path));
     }
 }
