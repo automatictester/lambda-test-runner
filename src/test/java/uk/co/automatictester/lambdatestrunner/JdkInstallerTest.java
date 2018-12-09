684e802e19b0ce4a665c8afb6e0819750137633c
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-12-09 13:03:11 +0000

    Revert "Switch JdkInstallerTest to debug"
    This reverts commit 95fa38082e0f253d20ac3f9b39875b3966e3c2c9.

--- src/test/java/uk/co/automatictester/lambdatestrunner/JdkInstallerTest.java
+++ src/test/java/uk/co/automatictester/lambdatestrunner/JdkInstallerTest.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
@@ -16,9 +15,7 @@ public class JdkInstallerTest {
 
     @Test(groups = "travis")
     public void testInstallJdk() {
-        Map<String, String> extraEnvVars = new HashMap<>();
-        extraEnvVars.put("JAVA_HOME", "/tmp/jdk10");
-        extraEnvVars.put("LOG_LEVEL", "debug");
+        Map<String, String> extraEnvVars = Collections.singletonMap("JAVA_HOME", "/tmp/jdk10");
         ProcessResult processResult = JdkInstaller.installJdk(extraEnvVars);
         assertEquals(processResult.getExitCode(), 0);
         Path path = Paths.get("/tmp/jdk10");
